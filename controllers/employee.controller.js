dade4a874994022404f7a07339468aa12d5a1478
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-08-02 18:07:51 +0300

    fix

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -2,7 +2,6 @@ const db = require('../db');
 const {main} = require('../models/employee.model')
 
 class EmployeeController {
-    
     async getEmployees(req, res) {
         const variant = req.params.variant;
         const employees = await db.query('SELECT * FROM empl_list');
